2651006c49d4ad5edfdf4a2ea3cbb6e5bf1d7b59
unknown <williamsartijose@hotmail.com>
2022-09-21 15:43:10 -0300

    first commit

---
+++ backend/src/main/java/com/DevSuperior/dscatalog/DsCatalogDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.DevSuperior.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalogDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalogDevSuperiorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/DevSuperior/dscatalog/DsCatalogDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevSuperior.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
