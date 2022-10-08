7e8825718da73c4845bf4a414e7b05ff0eed512c
unknown <williamsartijose@hotmail.com>
2022-10-08 00:56:33 -0300

    POST new Category

--- backend/src/main/java/com/DevSuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/DevSuperior/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.DevSuperior.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.DevSuperior.dscatalog.dto.CategoryDTO;
 import com.DevSuperior.dscatalog.services.CategoryService;
@@ -31,7 +35,18 @@ public class CategoryResource {
  	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
       
  		CategoryDTO dto = service.findById(id);
-      return ResponseEntity.ok().body(dto);     
+      return ResponseEntity.ok().body(dto);   
+      
+ 	}
+ 	
+ 	
+ 	//INSERINDO DADOS COM O POST 201 criado
+ 	@PostMapping
+ 	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+ 		dto = service.insert(dto);
+ 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+ 				.buildAndExpand(dto.getId()).toUri();
+ 		return ResponseEntity.created(uri).body(dto);
      
 //EXEMPLO DE LISTA MOCADA 		
 //		List<Category> list = new ArrayList<>();
--- backend/src/main/java/com/DevSuperior/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/com/DevSuperior/dscatalog/resources/exceptions/StandardError.java
@@ -17,14 +17,6 @@ public class StandardError implements Serializable {
 		
 	}
 
-	public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
-		this.timestamp = timestamp;
-		this.status = status;
-		this.error = error;
-		this.message = message;
-		this.path = path;
-	}
-
 	public Instant getTimestamp() {
 		return timestamp;
 	}
